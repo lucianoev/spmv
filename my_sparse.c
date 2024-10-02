978cb57be8beafca6a43df0b00ddf85a3369f0c9
lucianoev <lucianovazquez@outlook.com>
2024-10-02 13:36:21 +0200

    Estructura para CSR en my_sparse

--- my_sparse.c
+++ my_sparse.c
@@ -1,6 +1,17 @@
 #include "spmv.h"
+#include "stdio.h"
 
-int my_sparse()
-{
-  // code your own solver
+
+//Estructura para matriz
+typedef struct{
+	int* fila_inicio;
+	int* indices_columnas;
+	double* val;
+	int num_filas;
+	int num_columnas;
+	int num_sin_ceros;
+} MatrizCSR;
+
+void my_sparse(){
+
 }
