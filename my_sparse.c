04737c8104e59b6c59fba7fe11afabee25c6cf6c
lucianoev <lucianovazquez@outlook.com>
2024-10-03 15:43:02 +0200

    Funcion producto matriz-vector en CSR

--- my_sparse.c
+++ my_sparse.c
@@ -12,6 +12,19 @@ typedef struct{
 	int num_sin_ceros;
 } MatrizCSR;
 
-void my_sparse(){
 
+//Función para el producto entre matriz y vector 
+void prod_matriz_vector(const MatrizCSR* matriz_csr, const double* vector, double* resultado){
+
+	//Inicializo vector
+	for (int fila = 0; fila < matriz_csr->num_filas; fila++) {
+	        resultado[fila] = 0.0;
+	}
+
+	//Producto de matriz CSR con vector
+	for (int fila = 0; fila < matriz_csr.num_filas; fila++) {
+		        for (int idx = matriz_csr.fila_inicio[fila]; idx < matriz_csr.fila_inicio[fila + 1]; idx++) {
+				            resultado[fila] += matriz_csr.valores[idx] * vector[matriz_csr.indices_columnas[idx]];    
+			}
+	}
 }
